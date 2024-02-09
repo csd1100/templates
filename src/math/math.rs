64a1ea606fd4637a9aaba2dcbb3170061085f643
Chaitanya Deshpande <46526862+csd1100@users.noreply.github.com>
2024-02-09 15:38:44 +0530

    feat(#13): add anyhow in rust template (#15)

--- src/math/math.rs
+++ src/math/math.rs
@@ -1,13 +1,40 @@
-pub fn add(num1: i32, num2: i32) -> i32 {
-    return num1 + num2;
+use anyhow::{anyhow, Result};
+
+pub fn add(num1: i32, num2: i32) -> Result<i32> {
+    return Ok(num1 + num2);
+}
+
+pub fn div(num1: i32, num2: i32) -> Result<i32> {
+    if num2 == 0 {
+        return Err(anyhow!("Cannot divide by zero"));
+    }
+
+    return Ok(num1 / num2);
 }
 
 #[cfg(test)]
 mod test {
-    use crate::math::math::add;
+    use anyhow::Result;
+
+    use crate::math::math::{add, div};
 
     #[test]
-    fn add_2_numbers() {
-        assert_eq!(2, add(1, 1));
+    fn add_2_numbers() -> Result<()> {
+        assert_eq!(2, add(1, 1)?);
+        return Ok(());
+    }
+
+    #[test]
+    fn div_2_numbers() -> Result<()> {
+        assert_eq!(2, div(4, 2)?);
+        return Ok(());
+    }
+
+    #[test]
+    fn div_by_zero() -> Result<()> {
+        let expected = String::from("Cannot divide by zero");
+        let actual = div(100, 0).unwrap_err().to_string();
+        assert_eq!(expected, actual);
+        return Ok(());
     }
 }
