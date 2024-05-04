ffaa0a574c2e03a0ad321d14efc592d2f4020a9b
Chaitanya Deshpande <cdeshpande@proton.me>
2024-05-04 10:45:01 +0530

    feat(#16): cleanup electron-react-ts branch

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/csd1100/templates
-
-go 1.21.6
--- internal/parsers/helper_test.go
+++
@@ -1,5 +0,0 @@
-package parsers_test
-
-const ERROR = "error"
-const VALUE = "value"
-const FAILURE_MESSAGE = "%s: assertion failed for %s \nExpected:\n\t %v \nActual:\n\t %v"
